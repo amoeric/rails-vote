d090e8a76f49f405567042b0906d2c8ff2e27c70
asyu eric <xeriok02390@gmail.com>
2019-05-22 11:49:30 +0800

    new_candidate_add_validation

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -7,11 +7,13 @@ class CandidatesController < ApplicationController
     def create
         @candidate = Candidate.new(candidate_params)
         
+        
+
         if @candidate.save
             flash[:notice] = '新增成功'
             redirect_to "/"
         else
-            flash[:notice] = '新增失敗'
+            
             render :new
         end
     end
