4a056edaab7a731009c90aa24afaff1907612fc4
Eric Syu <xeriok02390@gmail.com>
2019-06-10 14:27:08 +0800

    pg runsuccess

--- db/migrate/20190610025730_change_default.rb
+++ db/migrate/20190610025730_change_default.rb
@@ -1,5 +1,5 @@
 class ChangeDefault < ActiveRecord::Migration[5.2]
   def change
-    change_colum_default :orders, :state, 'pending'
+    change_column_default :orders, :state, 'pending'
   end
 end
